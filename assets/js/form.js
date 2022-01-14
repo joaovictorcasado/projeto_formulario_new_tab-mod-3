70bae793d70e395dd5a58ee841ea3e5adf29e68a
joaovictorcasado <joaovictorcasado@hotmail.com>
2022-01-14 14:19:58 -0300

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,98 @@
+function testaFormulario(e){
+    e.preventDefault();
+
+    // for(i in e.target.elements['tel'].value){
+    //   if  ('0123456789'.indexOf(e.target.elements['tel'].value[i]) == -1) {
+    //     alert('Apenas números são permitidos no campo telefone!')  
+    //     return false
+    //   }
+    // }
+
+    //var temLetras = e.target.elements['tel'].value.match(/[a-zA-Z]/g)
+    var telPattern = /[^0-9-() ]+/g
+
+    if (telPattern.test(e.target.elements['tel'].value)) {
+    //if( temLetras && temLetras.length) {
+        alert('Apenas números são permitidos no campo telefone!')  
+        return false
+    }
+
+    if(e.target.elements['tel'].value.replace(/[-() ]/g, '').length < 11){
+        alert('Números inválido!')  
+        return false
+    }
+
+    var pessoasRaw = localStorage.getItem('pessoas')
+     if(pessoasRaw != null){
+        var pessoas = JSON.parse(pessoasRaw)
+    }else{
+        var pessoas = [];
+    }     
+    
+    if(id !==null){
+        pessoas[id] = {
+         name: e.target.elements['name'].value  ,
+         tel:  e.target.elements['tel'].value ,
+         exp:  (e.target.elements['exp'].value=='true')
+         }
+
+    } else {
+        pessoas.push({
+         name: e.target.elements['name'].value  ,
+         tel:  e.target.elements['tel'].value ,
+         exp:  (e.target.elements['exp'].value=='true')
+    })
+}
+    localStorage.setItem('pessoas', JSON.stringify(pessoas))
+ 
+    document.getElementById('goHome').click(); 
+}
+ var urlPrincipal = new URL(window.location.href)
+
+var id = urlPrincipal.searchParams.get('person')
+if(id !==null){
+        var pessoasRaw = localStorage.getItem('pessoas')
+     if(pessoasRaw != null){
+        var pessoas = JSON.parse(pessoasRaw)
+    }else{
+        var pessoas = [];
+    }     
+    
+    console.log(pessoas[id])
+    document.getElementById('name').value = pessoas[id].name
+    document.getElementById('tel').value = pessoas[id].tel
+    if(pessoas[id].exp){
+        document.getElementById('exp-yes').checked = true
+    }else{
+        document.getElementById('exp-no').checked = true
+    }
+}
+function testaCampoTelefone(e){
+    
+    // var telPattern = /[^0-9-() ]+/g
+    // if (telPattern.test(texto)) {
+    //     alert('Apenas números são permitidos no campo telefone!')  
+    //     return false
+    // }
+
+    // if(e.target.elements['tel'].value.replace(/[-() ]/g, '').length < 11){
+    //     alert('Números inválido!')  
+    //     return false
+    // }
+    e.preventDefault()
+    console.log(e)
+
+    if(e.target.value.length == 0) {
+        e.target.value += '('
+    }
+    if(e.target.value.length == 3) {
+        e.target.value += ') '
+    }
+    if(e.target.value.length == 10) {
+        e.target.value += '-'
+    }
+
+    if ((/[0-9]/g).test(e.key) && e.target.value.length < 15) {
+        e.target.value += e.key
+    }
+}
